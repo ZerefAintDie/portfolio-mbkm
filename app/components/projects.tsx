27500dfc14ff0582f3481aaee07959ca5ec139d2
Muhammad Althafino <malthafino36@gmail.com>
2025-12-25 15:31:03 +0700

    fixing error

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -26,22 +26,22 @@ export default function Projects() {
             src="/AI.png"
             alt="dekor 1"
             className="decor decor-1"
-            width={100}
-            height={100}
+            width={500}
+            height={500}
           />
           <Image
             src="/MDIDE.png"
             alt="dekor 2"
             className="decor decor-2"
-            width={100}
-            height={100}
+            width={500}
+            height={500}
           />
           <Image
             src="/ROSBD.png"
             alt="dekor 3"
             className="decor decor-3"
-            width={100}
-            height={100}
+            width={500}
+            height={500}
           />
         </div>
       </div>
@@ -70,7 +70,7 @@ export default function Projects() {
         </div>
         <div className="projek-content">
           <h3>Integrasi Pipeline Database End-to-End</h3>
-          <h4>Infrastruktur dan Platform Big Data</h4>
+          <h4>Database Management System</h4>
           <p>
             Projek ini berfokus pada pengolahan data besar (Big Data) menggunakan database konvensional dan penjadwalan dengan Airflow. Output akhir berupa dashboard untuk melakukan analisis tren film dari tahun ke tahun.
           </p>
@@ -87,7 +87,7 @@ export default function Projects() {
         </div>
         <div className="projek-content">
           <h3>Sistem Prediksi Harga Bitcoin</h3>
-          <h4>Rekayasa dan Organisasi Sistem Big Data</h4>
+          <h4>Pengolahan Data Streaming</h4>
           <p>
             Proyek ini berfokus pada pengolahan data yang mengalir secara real-time, di mana data diperoleh langsung dari sumber online dan diproses secara berkelanjutan oleh sistem. Data yang masuk dianalisis secara otomatis untuk menghasilkan insight dan prediksi yang relevan, kemudian disajikan dalam bentuk dashboard visual yang mudah dipahami sehingga pengguna dapat memantau kondisi dan tren secara cepat dan informatif.
           </p>
@@ -112,6 +112,44 @@ export default function Projects() {
           <span>Scikit Learn</span>
           <span>NLTK</span>
           <span>Apify</span>
+        </div>
+
+      </div>
+
+      {/* PROJECT 5 */}
+      <div className="projek-item" data-aos="fade-up">
+        <div className="projek-image">
+          <img src="/KBR.png" alt="Projek 5" />
+        </div>
+        <div className="projek-content">
+          <h3>Sistem Pengetahuan dan Penalaran</h3>
+          <h4>Semantic Web</h4>
+          <p>
+            Tax Ontology (PPh 21 & PPh 26) adalah proyek yang bertujuan memodelkan aturan pajak penghasilan di Indonesia ke dalam sebuah sistem yang terstruktur dan mudah dipahami, dengan mengelompokkan informasi seperti wajib pajak, jenis penghasilan, dan tarif pajak agar dapat dianalisis secara otomatis. Sistem ini menyediakan tampilan interaktif yang memungkinkan pengguna menelusuri data, memahami klasifikasi wajib pajak, dan melihat hasil analisis aturan pajak secara lebih sederhana untuk mendukung pembelajaran dan pemahaman kebijakan perpajakan.
+
+          </p>
+          <span>Protege</span>
+          <span>Apache Jena Fuseki</span>
+          <span>Streamlit</span>
+        </div>
+
+      </div>
+
+      {/* PROJECT 6 */}
+      <div className="projek-item" data-aos="fade-up">
+        <div className="projek-image">
+          <img src="/ERP.png" alt="Projek 6" />
+        </div>
+        <div className="projek-content">
+          <h3>Aplikasi Enterprise Resource Planning</h3>
+          <h4>Web Application</h4>
+          <p>
+            Website ERP (Enterprise Resource Planning) yang dirancang untuk membantu bisnis—terutama UMKM seperti F&B, retail, dan manufaktur—dalam mengelola operasional harian secara terpusat dan efisien. Melalui website ini, pengguna dapat mengatur data produk, stok barang, supplier, serta mencatat transaksi dan pembelian dalam satu sistem yang terintegrasi. Website ini juga dilengkapi dengan dashboard ringkas yang menyajikan gambaran kondisi bisnis, seperti pergerakan stok dan performa penjualan, sehingga membantu pemilik usaha maupun tim operasional dalam mengambil keputusan yang lebih cepat dan terstruktur.
+
+          </p>
+          <span>Database</span>
+          <span>React</span>
+          <span>JavaScript</span>
         </div>
 
       </div>
